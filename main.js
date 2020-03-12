7aef3a17fc205208039f4f54d9379baa09863626
Ubuntu <ubuntu@ip-172-31-17-161.us-east-2.compute.internal>
2020-03-12 16:26:28 +0000

    main.js updated

--- main.js
+++ main.js
@@ -5,14 +5,8 @@
       var MARKER_PATH = 'https://developers.google.com/maps/documentation/javascript/images/marker_green';
       var hostnameRegexp = new RegExp('^https?://.+?/');
       var searchOption=["cafe"]
-      function changeOption (){
-        document.getElementById("types");
-        types:[types.option[types.selectedIndex].value];
-        return types;
-      }
-     
-
-      var countries = {
+    
+       var countries = {
         'au': {
           center: {lat: -25.3, lng: 133.8},
           zoom: 4
